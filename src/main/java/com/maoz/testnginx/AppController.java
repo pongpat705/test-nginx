fa2c737a7dfd7094ce41eb3be47a5818e311a9ca
fhluky <fhluky@gmail.com>
2019-10-31 16:41:31 +0700

    print header

--- src/main/java/com/maoz/testnginx/AppController.java
+++ src/main/java/com/maoz/testnginx/AppController.java
@@ -3,6 +3,7 @@ package com.maoz.testnginx;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,9 +11,16 @@ import java.util.Map;
 public class AppController {
 
     @RequestMapping(value = "/")
-    public Map<String, Object> index(){
+    public Map<String, Object> index(HttpServletRequest request){
+
+
+
         Map<String, Object> x = new HashMap<>();
         x.put("status", 200);
+        while (request.getHeaderNames().hasMoreElements()){
+            String k = request.getHeaderNames().nextElement();
+            x.put(k, request.getHeader(k));
+        }
 
         return x;
     }
